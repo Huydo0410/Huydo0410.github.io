087c6aa6c8a4f2c03ab9792656c1f1c938845c20
HuyThuong <huydo0410gmail.com>
2018-01-10 11:13:27 +0700

    up lan dau

---
+++ project tốt nghiệp/deco.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.click').click(function(){
+		swal("Không tìm thấy sách", "...chúc bạn may mắn lần sau!");
+	})
+	$('.carousel').carousel({
+  interval: 2000
+})
+	$('.sc_emailer_input2').click(function(){
+		swal("Đăng kí thành công")
+	})
+ 
+})
+
+
