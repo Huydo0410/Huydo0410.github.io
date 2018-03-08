9fe4b128aed77b238af25c17a13e53beaf700ce3
Huy Đỗ Quang <huydo0410@gmail.com>
2018-03-08 21:38:02 +0700

    1

--- project/deco.js
+++ project/deco.js
@@ -1,19 +1,21 @@
-$(function(){
-	$('.click').click(function(){
-		swal("Không tìm thấy sách", "...chúc bạn may mắn lần sau!");
-	})
-	$('.carousel').carousel({
-  interval: 2000
-})
-	$('.sc_emailer_input2').click(function(){
-		swal("Đăng kí thành công")
-	})
- 
+
+$('.click').click(function(){
+	swal("not found!", "...try again!");
 });
+$('.carousel').carousel({
+	interval: 2000
+});
+$('.btn-outline-success').click(function(){
+	swal("Đăng kí thành công")
+});
+
 
 $('#book-hover').hover(function(){
 	$('.hover-img').addClass('test')
 });
 $('#book-hover').mouseleave(function(){
 	$('.hover-img').removeClass('test')
-})
+});
+
+
+
